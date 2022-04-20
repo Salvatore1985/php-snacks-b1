df21239c231aeff780eafcc5431f9a572c0b539f
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-04-20 17:07:22 +0200

    add style

--- snacks6/index.php
+++ snacks6/index.php
@@ -4,7 +4,32 @@ Utilizzare questo array: https://pastebin.com/CkX3680A.
 Stampiamo il nostro array mettendo gli insegnanti in un rettangolo 
 grigio e i PM in un rettangolo verde.*/
 
+
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
 ?>
+
 
 
 
@@ -16,12 +41,21 @@ grigio e i PM in un rettangolo verde.*/
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>php-snacks-6</title>
 </head>
 
 <body>
     <h1>php-snacks-6</h1>
+    <section class="my-flex">
 
+        <div class="box gray">
+
+        </div>
+        <div class="box green">
+
+        </div>
+    </section>
 
 </body>
 
