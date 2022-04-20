e640b188ce8f724d747d444d5fd34899638ed129
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-04-20 16:50:58 +0200

    intro snack6

--- snacks6/index.php
+++ snacks6/index.php
@@ -16,39 +16,12 @@ grigio e i PM in un rettangolo verde.*/
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-snacks-3</title>
+    <title>php-snacks-6</title>
 </head>
 
 <body>
-    <h1>php-snacks-3</h1>
-    <ul>
-        <?php
-        // *salvo il valore dell'array
-        $postsValues = array_values($posts);
-        // *salvo le chiavi dell'array
-        $postsKeys = array_keys($posts);
+    <h1>php-snacks-6</h1>
 
-        for ($i = 0; $i < count($postsKeys); $i++) { ?>
-            <h4>Post in data <?php echo $postsKeys[$i] ?></h4>
-            <?php for ($j = 0; $j < count($postsValues[$i]); $j++) { ?>
-                <h3>
-                    <?php echo $postsValues[$i][$j]['title'] ?>
-                </h3>
-                <h4>
-                    <em>
-                        <?php echo $postsValues[$i][$j]['author'] ?>
-
-                    </em>
-                </h4>
-                <pre>
-                    <?php echo $postsValues[$i][$j]['text'] ?>
-                </pre>
-
-            <?php } ?>
-        <?php } ?>
-
-
-    </ul>
 
 </body>
 
